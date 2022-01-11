17e29b8ce41f09957ae4cb81fe188b2ca8f444be
moni-tu <turani.monica@gmail.com>
2022-01-11 21:14:02 +0100

    re-factor: ass the movie object from each iteration of the map() function to <MovieCard key={movie._id} />
    As for using movie data within the MovieCard component, you first need to pass the movie object from each iteration of the map() function to <MovieCard key={movie._id} />. This is done by adding a custom attribute before /> and setting its value to movie, for example, movieData={movie}

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -22,7 +22,7 @@ export class MainView extends React.Component {
 
   return (
     <div className="main-view">
-      {movies.map(movie => <MovieCard key={movie._id} />)}
+      {movies.map(movie => <MovieCard key={movie._id} movieData={movie} />)}
     </div>
   );
 }
