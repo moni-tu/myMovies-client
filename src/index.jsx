380dfdedbfaa3ef701bb17010b19148f62894093
moni-tu <turani.monica@gmail.com>
2022-01-11 12:06:25 +0100

    Fix app name

--- src/index.jsx
+++ src/index.jsx
@@ -5,10 +5,10 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 
 // Main component (will eventually use all the others)
-class MyFlixApplication extends React.Component {
+class myMoviesApplication extends React.Component {
   render() {
     return (
-      <div className="my-flix">
+      <div className="my-movies">
         <div>Good morning</div>
       </div>
     );
@@ -19,4 +19,4 @@ class MyFlixApplication extends React.Component {
 const container = document.getElementsByClassName('app-container')[0];
 
 // Tells React to render your app in the root DOM element
-ReactDOM.render(React.createElement(MyFlixApplication), container);
+ReactDOM.render(React.createElement(myMoviesApplication), container);
