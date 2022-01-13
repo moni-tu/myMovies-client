c926e7c78d404acb88abfe38d4723198bab25719
moni-tu <turani.monica@gmail.com>
2022-01-13 19:45:39 +0100

    re-factor: delete button and add event click
    change the selectedMovie state when you click MovieCard

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -27,8 +27,8 @@ export class MainView extends React.Component {
 
     return (
     <div className="main-view">
-      <button onClick={() => {alert('Nice!')}}>Click me!</button>
-      {movies.map(movie => <MovieCard key={movie._id} movieData={movie} />)}
+      
+      {movies.map(movie => <MovieCard key={movie._id} movieData={movie} onClick={() => { this.state.selectedMovie = movie; }}/>)}
     </div>
   );
 }
