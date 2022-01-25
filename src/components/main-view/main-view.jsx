f9a56a273c113c65806286227d7798da8f02860e
moni-tu <turani.monica@gmail.com>
2022-01-25 15:56:15 +0100

    fix: change to small letter instead of capital

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -20,7 +20,7 @@ export class MainView extends React.Component {
       }
 
 componentDidMount(){
-  axios.get('https://mymovie-backend-api.herokuapp.com/myMovies')
+  axios.get('https://mymovie-backend-api.herokuapp.com/mymovies')
     .then(response => {
       this.setState({
         movies: response.data
