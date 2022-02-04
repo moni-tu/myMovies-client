4fc1b40631be1d5afbd5f0b4342b808e2ef7c4b2
moni-tu <turani.monica@gmail.com>
2022-02-04 22:43:45 +0100

    add React classes

--- src/components/movie-view/movie-view.jsx
+++ src/components/movie-view/movie-view.jsx
@@ -7,6 +7,19 @@ import axios from 'axios';
 import './movie-view.scss';
 
 export class MovieView extends React.Component {
+  /* constructor(props) {
+    super(props);
+    // Create state variables that will be used to add/remove a movie from a users Favorites list
+    this.state = {
+      
+      FavoriteMovies: [],
+      userDetails: []
+  }
+
+    // Bind these additional functions that will get called by onClick events to 'this'
+    this.addFavorite = this.addFavorite.bind(this);
+    this.removeFavorite = this.removeFavorite.bind(this);
+  } */
 
   keypressCallback(event) {
     console.log(event.key);
@@ -24,42 +37,38 @@ export class MovieView extends React.Component {
     const { movie, onBackClick } = this.props;
 
     return (
-      <div className="movie-view">
-        <div className="movie-poster">
-          <img crossOrigin='anonymous' src={movie.imagePath} />
-        </div>
-        <div className="movie-title">
-          <span className="label">Title: </span>
-          <span className="value">{movie.title}</span>
-        </div>
-        <div className="movie-description">
-          <span className="label">Description: </span>{"\n"}
-          <span className="value">{movie.description}</span>
-        </div>
-        <div className="movie-genre">
-          <span className="label">Genre: </span>
-          <span className="value">{movie.genre.name}</span>
-          <div>
-            <span className="label">Description: </span>
-            <span className="value">{movie.genre.description}</span>
-          </div>
-        </div>
-        <div className="movie-director">
-          <span className="label">Director: </span>
-          <span className="value">{movie.director.name}</span>
-          <div>
-            <span className="label">Bio: </span>
-            <span className="value">{movie.director.bio}</span>
-          </div>
-          <div>
-            <span className="label">Birth Date: </span>
-            <span className="value">{movie.director.birth}</span>
-          </div>
-        </div>
-
-        <button onClick={() => { onBackClick(null); }}>Back</button>
-      </div>
-
+      <Card bg="secondary" text="light" border="light">
+          <Card.Body>
+              <Row>
+                  <Col xs={12} md={6}>
+                      <Card.Img varient="top" src={movie.imagePath} className="big_image" />
+                  </Col>
+                  <Col xs={12} md={6}>
+                      <Card.Title className="text-center">{movie.title}</Card.Title>
+                      <Card.Text>{movie.description}</Card.Text>
+                      {/* && operator here is an alternative to an if statement. If movie.Genre.Name exists, then it will render the Genre section. If it doesn't exist, it will skip */}
+                      {movie.genre.name && (
+                          <Card.Text className="genre_heading"><span className="genre_title">Genre: </span><Link style={{ color: "white" }} to={`/genres/${movie.genre.name}`}>{movie.genre.name}</Link></Card.Text>
+                      )}
+                      {movie.director.name && (
+                          <Card.Text className="director_heading"><span className="director_title">Director: </span><Link style={{ color: "white" }} to={`/directors/${movie.director.name}`}>{movie.director.name}</Link></Card.Text>
+                      )}
+                      <Button onClick={() => onBackClick(null)} variant="light" style={{ color: "white" }}>Back</Button>
+                      {/* Use flag defined above to determine if we need an add or remove from Favorites button */}
+                      {isFavoriteNew ? (
+                          <Button className="float-right" variant="light" style={{ color: "white" }} onClick={this.removeFavorite}>
+                              Remove from Favorites
+                          </Button>
+                      ) : (
+                          <Button className="float-right" variant="light" style={{ color: "white" }} onClick={this.addFavorite}>
+                              Add to Favorites
+                          </Button>
+                      )}
+                      
+                  </Col>
+              </Row>
+          </Card.Body>
+      </Card>
     );
   }
 }
