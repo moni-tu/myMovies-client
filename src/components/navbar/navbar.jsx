b52f647f2440f88550ec04fdbee56eb985c33dc2
moni-tu <turani.monica@gmail.com>
2022-02-09 14:54:54 +0100

    import Reacr, react bootstrap and add return statement with navbar elements

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,6 +1,22 @@
 import React from 'react';
-import { Navbar, Container, Nav, Button } from 'react-bootstrap';
+import { Navbar, Container, Nav, Button, NavbarBrand } from 'react-bootstrap';
+import NavbarCollapse from 'react-bootstrap/esm/NavbarCollapse';
+import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 
 export function Menubar({user}){ 
-    
+
+    return (
+        <Navbar>
+            <Container>
+                <NavbarBrand> MyMovies </NavbarBrand>
+                <NavbarToggle/>
+                <NavbarCollapse>
+                    <Nav>
+                        
+                    </Nav>
+                </NavbarCollapse>
+
+            </Container>
+        </Navbar>
+    );
  }
