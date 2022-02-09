0c7f78ea3e6e5ffa7a9787c2044fd0cf327f0441
moni-tu <turani.monica@gmail.com>
2022-02-09 15:02:07 +0100

    create method to return token and hide signup page if user is already registered
    Create a method that returns a token from local storage; the else function should return false. Use this method to hide the signup page if the token exists and to display home, profile, and signout.

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,18 +5,42 @@ import NavbarToggle from 'react-bootstrap/esm/NavbarToggle';
 
 export function Menubar({user}){ 
 
+    const isAuth = () => {
+        if (typeof window == "undefined") {
+            return false;
+        }
+        if (localStorage.getItem("token")) {
+            return localStorage.getItem("token");
+        } else {
+            return false;
+        }
+    };
+
     return (
         <Navbar className="main-nav" sticky="top" expand="lg" variant="dark">
             <Container fluid>
                 <NavbarBrand lassName="navbar-logo" href="/"> MyMovies </NavbarBrand>
                 <NavbarToggle aria-controls="responsive-navbar-nav"/>
                 <NavbarCollapse id="responsive-navbar-nav">
-                    <Nav className="ml-auto">                    
-
+                    <Nav className="ml-auto">  
+                    {isAuth() && (
+                            <Nav.Link href="/profile">{user}</Nav.Link>
+                        )}
+                        {isAuth() && (
+                            <Button variant="link" onClick={() => {
+                                onLoggedOut()
+                            }}>Logout</Button>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/">Login</Nav.Link>
+                        )}
+                        {!isAuth() && (
+                            <Nav.Link href="/register">Register</Nav.Link>
+                        )}                  
                     </Nav>
                 </NavbarCollapse>
 
             </Container>
         </Navbar>
     );
- }
+}
